a4d1e450e2c19214197bfaf33a75be6c97814ff7
NisalM <nmmuthugala@gmail.com>
2020-02-19 22:47:05 +0530

    Initial Commit

---
+++ Financial Calculator/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Financial Calculator
+//
+//  Created by Nisal Muthugala on 2/19/20.
+//  Copyright © 2020 Nisal Muthugala. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Financial Calculator/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Financial Calculator
+//
+//  Created by Nisal Muthugala on 2/19/20.
+//  Copyright © 2020 Nisal Muthugala. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
